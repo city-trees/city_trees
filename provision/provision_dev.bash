f3300f10f97e33e028f5e3a684bdab23dee5a3cc
chyzwar <schizek.marcin@gmail.com>
2022-03-27 21:49:20 +0200

    chore(): update provision

--- provision/provision_dev.bash
+++ provision/provision_dev.bash
@@ -9,6 +9,15 @@ sudo systemctl enable city-trees.service
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install postgresql postgresql-contrib -y
-sudo systemctl start postgresql.service
+sudo apt install gnupg2 wget vim -y
 
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt -y update
+sudo apt -y install postgresql-14
+
+sudo -u postgres psql << EOF
+  CREATE DATABASE city_trees;
+  CREATE USER city_trees WITH ENCRYPTED PASSWORD 'city_trees';
+  GRANT ALL PRIVILEGES ON DATABASE city_trees TO city_trees;
+EOF
--- provision/provision_prod.bash
+++ provision/provision_prod.bash
@@ -6,3 +6,18 @@ sudo cp -f ./provision/city-trees.service /etc/systemd/system/city-trees.service
 sudo systemctl daemon-reload
 sudo systemctl start city-trees.service
 sudo systemctl enable city-trees.service
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install gnupg2 wget vim -y
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt -y update
+sudo apt -y install postgresql-14
+
+sudo -u postgres psql << EOF
+  CREATE DATABASE city_trees;
+  CREATE USER city_trees WITH ENCRYPTED PASSWORD 'city_trees';
+  GRANT ALL PRIVILEGES ON DATABASE city_trees TO city_trees;
+EOF
