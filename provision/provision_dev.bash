14fa1cb5bd6e7bd097e938b1a26a512e9007f185
chyzwar <schizek.marcin@gmail.com>
2022-03-27 20:48:35 +0200

    chore(): fix service name

--- provision/provision_dev.bash
+++ provision/provision_dev.bash
@@ -6,3 +6,9 @@ sudo cp -f ./provision/city-trees.service /etc/systemd/system/city-trees.service
 sudo systemctl daemon-reload
 sudo systemctl start city-trees.service
 sudo systemctl enable city-trees.service
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install postgresql postgresql-contrib -y
+sudo systemctl start postgresql.service
+
