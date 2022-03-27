b1f6dfaf638036dd247739ba3270622cdaf01854
chyzwar <schizek.marcin@gmail.com>
2022-03-27 14:25:43 +0200

    chore(): script

--- provision/provision_dev.bash
+++ provision/provision_dev.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
 DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 cp "${DIR}"/city-trees.service /etc/systemd/system/city-trees.service
 
-systemctl daemon-reload
-systemctl start city-trees.service
-systemctl enable example.service
+sudo systemctl daemon-reload
+sudo systemctl start city-trees.service
+sudo systemctl enable example.service
