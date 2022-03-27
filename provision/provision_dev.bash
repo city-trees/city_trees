0fd44b3bd4b1b3b1c103de2f7fea03dc437b4b39
chyzwar <schizek.marcin@gmail.com>
2022-03-27 14:30:08 +0200

    chore(): script

--- provision/provision_dev.bash
+++ provision/provision_dev.bash
@@ -4,7 +4,7 @@ set -e
 
 DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-cp "${DIR}"/city-trees.service /etc/systemd/system/city-trees.service
+sudo cp -f ./provision/city-trees.service /etc/systemd/system/city-trees.service
 
 sudo systemctl daemon-reload
 sudo systemctl start city-trees.service
