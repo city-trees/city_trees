0b7e560dbbf5d71d685c44955dfc8b922dc93207
chyzwar <schizek.marcin@gmail.com>
2022-04-02 22:27:59 +0200

    chore(): no need to suod for pg

--- provision/pg-backup.bash
+++ provision/pg-backup.bash
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 # Create backup directory
-sudo mkdir -p /var/city_trees_backups
+mkdir -p /var/city_trees_backups
 
 # Declare backup filename
 BACKUP_FILENAME=$(date -I)_city_tree_db.sql
@@ -22,4 +22,4 @@ pg_dump \
 
 
 # Remove old backups, keep only 10 most recent
-sudo ls -dt /var/city_trees_backups/* | tail -n +11 | xargs rm -rf
+ls -dt /var/city_trees_backups/* | tail -n +11 | xargs rm -rf
